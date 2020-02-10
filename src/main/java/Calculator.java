4e9d38b3b117d06ac715a73b959c2e06e1e2839b
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-02-10 15:57:22 +0000

    calculator getters for name passing, add function tested

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,14 @@
 public class Calculator {
+    private String name;
+
+    public Calculator(String inputName){
+        this.name = inputName;
+    }
+    public int add(int valueOne, int valueTwo){
+        return valueOne + valueTwo;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class CalculatorTest {
     private Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator("Super Calculator");
+    }
+    @Test
+    public void check_calculator_has_name(){
+        assertEquals("Super Calculator", calculator.getName());
+    }
+
+    @Test
+    public void check_calculator_can_add_two_numbers_together(){
+        assertEquals(3, calculator.add(1,2));
+    }
+
 }
