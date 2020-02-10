eb847c23dcc29ac6890d1613982b8d627dd06032
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-02-10 16:13:41 +0000

    water bottle test for getting name and also volume starting at 100

---
+++ src/main/java/WaterBottle.java
@@ -0,0 +1,17 @@
+public class WaterBottle {
+    private String name;
+    private Integer volume;
+
+    public WaterBottle(String name) {
+        this.name = name;
+        this.volume = 100;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+}
---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class WaterBottleTest {
+    private WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle("Andrews Water Bottle");
+    }
+
+    @Test
+    public void can_get_water_bottle_name(){
+        assertEquals("Andrews Water Bottle", waterBottle.getName());
+    }
+
+    @Test
+    public void check_water_bottle_starts_at_100(){
+        assertEquals(100, waterBottle.getVolume().intValue());
+    }
+}
